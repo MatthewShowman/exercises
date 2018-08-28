e7baa319c02f46ed5e6e026ce7e9be4cb1237ae8
Matthew T Showman <matthew.t.showman@gmail.com>
2018-08-27 20:26:13 -0500

    Added in-class exercises from Aug 27th

---
+++ basic-javascript/introduction/sample-code.js
@@ -0,0 +1,20 @@
+//Print out "Hello World".
+console.log('Hello World');
+
+let myName = 'Matthew';
+let lastName = 'Showman';
+
+console.log(myName + " " + lastName);
+
+//Print out some numbers.
+let x = 5;
+let y = x;
+y = 6;
+
+x++;
+y--;
+
+console.log(x + y);
+console.log(y);
+
+console.log(typeof x);
