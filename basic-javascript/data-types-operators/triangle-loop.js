b1dea43a9b17179a2c0e61e39b032e98043e7809
Matthew T Showman <matthew.t.showman@gmail.com>
2018-09-05 17:22:38 -0500

    Edited the triangle loop

--- basic-javascript/data-types-operators/triangle-loop.js
+++ basic-javascript/data-types-operators/triangle-loop.js
@@ -1,10 +1,10 @@
 //Create a trianlge with #
 
-let i = 1;
-let myHash = "#";
+let i = 0;
+let myHash = "";
 
-while (i <= 7) {
-    console.log(myHash);
+while (i <= 6) {
     myHash = myHash + "#";
+    console.log(myHash);
     i++;
 }
