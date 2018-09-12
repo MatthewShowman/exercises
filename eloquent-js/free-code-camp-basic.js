b95b3d295560eaf9e1107b432059a9e62ab92aee
Matthew T Showman <matthew.t.showman@gmail.com>
2018-09-12 17:35:47 -0500

    Finished cfreeCodeCamp basic

--- eloquent-js/free-code-camp-basic.js
+++ eloquent-js/free-code-camp-basic.js
@@ -82,7 +82,7 @@ console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
 console.log(multiplyAll([[1],[2],[3]]));
 console.log(multiplyAll([[5,1],[0.2, 4, 0.5],[3, 9]]));
 console.log(multiplyAll([[7,3],[2]]));
-*/
+
 
 
 // Profile Lookup
@@ -135,3 +135,21 @@ function lookUpProfile(name, prop){
 // Change these values to test your function
 console.log(lookUpProfile("Akira", "likes"));
 console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Sherlock", "likes"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Bob", "number"));
+console.log(lookUpProfile("Bob", "potato"));
+console.log(lookUpProfile("Akira", "address"));
+*/
+
+
+
+//Multiple Conditional (Ternary) Operators
+
+function checkSign(num) {
+  return (num === 0) ? "zero" : (num > 0) ? "positive" : "negative";
+}
+
+console. log(checkSign(10));
+console. log(checkSign(-12));
+console. log(checkSign(0));
