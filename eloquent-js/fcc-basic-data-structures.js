ff6c7baac11dc8ccc35841496e32d9f2a3e630c7
Matthew Showman <matthew.t.showman@gmail.com>
2018-09-16 09:47:02 -0500

    Added a freeCodeCamp exercise

--- eloquent-js/fcc-basic-data-structures.js
+++ eloquent-js/fcc-basic-data-structures.js
@@ -46,3 +46,31 @@ function filteredArray(arr, elem) {
   console.log(myNestedArray[1][6][0]);
   console.log(myNestedArray[2][5][0][0]);
   console.log(myNestedArray[3][5][0][0][1]);
+
+
+
+  // Access Property Names with Bracket Notation
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  // do not change code above this line
+  
+  function checkInventory(scannedItem) {
+    return foods[scannedItem];
+    
+    // change code below this line
+  
+  }
+  
+  // change code below this line to test different cases:
+  console.log(checkInventory("apples"));
+
+
+
+
+  
