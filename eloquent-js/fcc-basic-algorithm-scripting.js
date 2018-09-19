520e26258bb17f13a87795e84996ef267d0d1c38
Matthew Showman <mshowman@uark.edu>
2018-09-19 12:46:29 -0500

    More FCC exercises

--- eloquent-js/fcc-basic-algorithm-scripting.js
+++ eloquent-js/fcc-basic-algorithm-scripting.js
@@ -24,7 +24,7 @@ function largestOfFour(arr) {
 
 
 
-  // Return Largest Numbers in Arrays 1.1
+  // Return Largest Numbers in Arrays 2.0
 
 function largestOfFourB(arr) {
   // You can do this!
@@ -65,5 +65,73 @@ console.log(confirmEnding("Open sesame", "same"));
 
 
 
-// 
+// Repeat a String Repeat a String 1.0
+// Repeat a given string str (first argument) for num times (second argument)
+// Return an empty string if num is not a positive number.
 
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  let repeatedString = "";
+
+  if (num <= 0) {
+    repeatedString;
+  } else {
+    for (let i = 0; i < num; i++) {
+      repeatedString += str;
+    }
+  }
+
+  return repeatedString;
+}
+
+console.log(repeatStringNumTimes("abc", 3));
+console.log(repeatStringNumTimes("*", 3));
+console.log(repeatStringNumTimes("&", -2));
+
+
+
+
+// Truncate a String
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
+// Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  let truncatedString = "";
+
+  if (str.length <= num) {
+    truncatedString = str;
+  } else {
+    truncatedString = str.substring(0,num) + "...";
+  }
+  return truncatedString;
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11));
+console.log(truncateString("A-", 1));
+
+
+
+
+// Finders Keepers
+// Create a function that looks through an array (first argument)
+// and returns the first element in the array that passes a truth test (second argument).
+// If no element passes the test, return undefined.
+
+// console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })); // 8
+// console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })); //undefined
+
+function findElement(arr, func) {
+   let num = 0;
+   for (let i = 0; i < arr.length; i++) {
+     if (func(arr[i])) {
+      num = arr[i];
+     } else {
+      num = undefined;
+     }
+   }
+   return num;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
