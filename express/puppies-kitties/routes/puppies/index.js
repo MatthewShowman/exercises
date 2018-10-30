6d69b38fcfc3e500c947b03877fcb50ee6351751
Matthew Showman <matthew.t.showman@gmail.com>
2018-10-29 21:18:06 -0500

    puppies

--- express/puppies-kitties/routes/puppies/index.js
+++ express/puppies-kitties/routes/puppies/index.js
@@ -38,4 +38,23 @@ router.get('/', /*refers to this directory*/ (req, res) => {
     res.send(puppies);
 });
 
+router.get('/:id', (req, res) => {
+  let foundPuppy = puppies.find(puppy => puppy.id == req.params.id);
+  res.send(foundPuppy);
+});
+
+router.post('/', (req, res) => {
+  let newPuppy = req.body;
+  puppies.push(newPuppy);
+  newPuppy.id = puppies.length;
+  res.send(newPuppy);
+});
+
+router.put('/:id', (req, res) => {
+  let puppyToUpdate = puppies.find(puppy => puppy.id == req.params.id);
+  puppyToUpdate.params = req.body.params
+  res.send(puppyToUpdate);
+  
+});
+
 module.exports = router;
