9e7efb10c516d47fd9ef204925d360fb58c97ef3
Matthew T Showman <matthew.t.showman@gmail.com>
2019-01-28 20:20:33 -0600

    tried to add flash message to tax calc but didn't work

--- express/tax-calculator/app.js
+++ express/tax-calculator/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const flash = require('connect-flash');
+const session = require('express-session');
 const routes = require('./routes');
 
 const app = express();
@@ -6,6 +8,13 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: ['bitter beaver', 'dangerous doggy', 'eager echidna'],
+    resave: true,
+    saveUninitialized: true,
+    // cookie: { secure: true },
+}));
+app.use(flash());
 
 app.use('/', routes);
 
--- express/tax-calculator/routes/index.js
+++ express/tax-calculator/routes/index.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
+
 const taxController = require('../controllers/tax.controller');
 
 router.get('/', (req, res) => {
     let taxes = {"tax" : 0};
-    res.render('home', {taxes});
+    res.render('home', {taxes, error: req.flash('error')});
 });
 
 router.post('/', (req, res) => {
     let income = req.body;
+    if(income < 0){
+        req.flash('error', 'Income cannot be negative');
+        return res.redirect("/");
+    }
     let taxes = {"tax" : taxController.taxCalculator(income)};
     res.render('home', {taxes});
 });
